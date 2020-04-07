1632a662c34bcb3ddadfe7b6383ab94c6c872d19
vhristov97 <59263596+vhristov97@users.noreply.github.com>
2020-04-07 20:35:46 +0200

    Dodata header komponenta

--- projekat/src/app/app.module.ts
+++ projekat/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/core/header/header.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent} from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
