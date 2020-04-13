414fb9f4070fb95a905c026cc90159db1e8660ce
vhristov97 <59263596+vhristov97@users.noreply.github.com>
2020-04-14 00:31:11 +0200

    Uradjene kartice i zapceta forma

--- projekat/src/app/pages/main/main.component.ts
+++ projekat/src/app/pages/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { City } from 'src/app/entities/city/city';
 import { CityService } from 'src/app/services/city/city.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -9,11 +10,24 @@ import { CityService } from 'src/app/services/city/city.service';
 })
 export class MainComponent implements OnInit {
   cities: City[];
+  flightForm: FormGroup;
 
   constructor(private cityService: CityService) { }
 
   ngOnInit(): void {
     this.cities = this.cityService.mockedCities();
+    this.initForm();
   }
 
+  initForm(){
+    this.flightForm = new FormGroup({
+      'origin': new FormControl(null, Validators.required),
+      'destination': new FormControl(null, Validators.required),
+      'from': new FormControl(null),
+      'to': new FormControl(null),
+      'passNum': new FormControl(1, Validators.required)
+    })
+  }
+
+  onSubmit(){}
 }
